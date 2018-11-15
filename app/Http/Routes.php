4361cd6d8388ac236084539262568846185af88a
Nelly Gabriella <schimko.nelly@gmail.com>
2018-11-15 19:26:41 +0100

    2018.11.15. 19:25 Adding Slug URLs to Our Blog

--- app/Http/Routes.php
+++
@@ -1,8 +0,0 @@
-Route::get('/', 'PagesController@getIndex');
-Route::get('hirek', 'PagesController@getNews');
-Route::get('allasok', 'PagesController@getJobs');
-Route::get('forum', 'PagesController@getQuestions');
-Route::get('jegyzetek', 'PagesController@getJobs');
-Route::get('kapcsolat', 'PagesController@getContact');
-Route::get('projektek', 'PagesController@getProjects');
-Route::resource('posts', 'PostController');
